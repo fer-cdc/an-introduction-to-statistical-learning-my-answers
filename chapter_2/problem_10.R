d29b4f168b51387d95f4800ce5221e5fc51fd547
fer-cdc <fercesardc@gmail.com>
2021-11-06 15:01:19 -0300

    10

--- chapter_2/problem_10.R
+++ chapter_2/problem_10.R
@@ -58,5 +58,53 @@ dim(Boston[chas == 1, ])[1]     # 35 suburbs
 median(ptratio)     # 19.05
 
 # (g)
-min_mdev = Boston[medv == min(medv), ]
+min_mdev = Boston[medv == min(medv), ]    # The census tract are number 399 and 406
+View(min_dev)
+
+#"What are the values of the other predictors for that census tract, and how do those values compare to the 
+# overall ranges for those predictors?""
+# To answer this question, I created a function in a file called "analysis.R" in which it compares each variable 
+# of a determined census tract to the Boston data. The output just answers if the variable's value are below or above certain 
+# quartile.
+
+comparison = dget("analysis.R")
+comparison_399 = comparison(min_mdev[1, ], Boston)     # min_dev[1, ] is the data of the census tract 399
+# Now we are going to combine it with the 399 data by using rbind function. We also transpose the data in order to make it
+# more clear to visualize.
+
+analysis_399 = t(rbind(min_mdev[1, ], comparison_399))
+#399       2                   
+#crim    "38.3518" "above 3rd quartile"
+#zn      "0"       "at min"            
+#indus   "18.1"    "at 3rd quartile"   
+#chas    "0"       "at min"            
+#nox     "0.693"   "above 3rd quartile"
+#rm      "5.453"   "below 1st quartile"
+#age     "100"     "at max"            
+#dis     "1.4896"  "below 1st quartile"
+#rad     "24"      "at 3rd quartile"   
+#tax     "666"     "at 3rd quartile"   
+#ptratio "20.2"    "at 3rd quartile"   
+#lstat   "30.59"   "above 3rd quartile"
+#medv    "5"       "at min"
+
+# By doing the same to the 406th census, we have:
+comparison_406 = comparison(min_mdev[2, ], Boston)
+analysis_406 = t(rbind(min_mdev[2, ], comparison_406))
+#406       2                   
+#crim    "67.9208" "above 3rd quartile"
+#zn      "0"       "at min"            
+#indus   "18.1"    "at 3rd quartile"   
+#chas    "0"       "at min"            
+#nox     "0.693"   "above 3rd quartile"
+#rm      "5.683"   "below 1st quartile"
+#age     "100"     "at max"            
+#dis     "1.4254"  "below 1st quartile"
+#rad     "24"      "at 3rd quartile"   
+#tax     "666"     "at 3rd quartile"   
+#ptratio "20.2"    "at 3rd quartile"   
+#lstat   "22.98"   "above 3rd quartile"
+#medv    "5"       "at min" 
+
+# (h)
 
